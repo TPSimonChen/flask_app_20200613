7ee4ce91207a9d44b872d89c6e5d9c830556610d
TPSimonChen <66862468+TPSimonChen@users.noreply.github.com>
2020-06-13 16:38:58 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -57,6 +57,25 @@ def getNews():
     tmp = title + ': ' +url
     return tmp
     
+def movie():
+    from bs4 import BeautifulSoup
+    import requests
+    
+    target_url = 'http://www.atmovies.com.tw/movie/next/0/'
+    print('Start parsing movie ...')
+    rs = requests.session()
+    res = rs.get(target_url, verify=False)
+    res.encoding = 'utf-8'
+    soup = BeautifulSoup(res.text, 'html.parser')
+    content = ""
+    for index, data in enumerate(soup.select('ul.filmListAll a')):
+        if index == 20:
+            break
+        title = data.text.replace('\t', '').replace('\r', '')
+        link = "http://www.atmovies.com.tw" + data['href']
+        content += '{}\n{}\n'.format(title, link)
+    return content
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # 傳送新聞
@@ -74,6 +93,9 @@ def handle_message(event):
             package_id='1',
             sticker_id='1'
         )
+    # 傳送電影 
+    elif event.message.text == '傳送電影':
+        message = TextSendMessage(movie()) 
     else:
         message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
